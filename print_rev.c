20e2610829d66c1505791efe227ee920e8d4c5ac
womamire <mihretutd@gmail.com>
2022-07-13 18:29:46 +0300

    print reverse update

--- print_rev.c
+++ print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * printf_srev - function that prints a str in reverse
+ * print_rev - function that prints a str in reverse
  * @args: type struct va_arg where is allocated printf arguments
  *
  * Return: the string
